cc761e115febf697bde10cee99a448c5516be702
Tomas Barton <barton.tomas@gmail.com>
2018-03-03 23:38:13 +0100

    fix path to io modules on debian 9

--- manifests/params.pp
+++ manifests/params.pp
@@ -10,8 +10,8 @@ class hindsight::params {
 
       case $::lsbdistcodename {
         'stretch': {
-          $io_lua_path = '/usr/lib/luasandbox/modules/?.lua;/usr/lib/x86_64-linux-gnu/lua/5.1/?.lua'
-          $io_lua_cpath = '/usr/lib/luasandbox/modules/?.so;/usr/lib/x86_64-linux-gnu/lua/5.1/?.so'
+          $io_lua_path = '/usr/lib/luasandbox/io_modules/?.lua;/usr/lib/x86_64-linux-gnu/lua/5.1/?.lua'
+          $io_lua_cpath = '/usr/lib/luasandbox/io_modules/?.so;/usr/lib/x86_64-linux-gnu/lua/5.1/?.so'
         }
         default: {
           $io_lua_path = '/usr/lib/luasandbox/io_modules/?.lua'
