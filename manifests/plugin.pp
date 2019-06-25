c33ec61cf409be83f764ba2a4b52d5528c955ee0
Kai S <kai@xs4all.nl>
2019-06-25 17:36:32 +0200

    Support a content or source argument for plugins (#2)
    * Support a content or source argument for plugins

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -10,6 +10,8 @@ define hindsight::plugin (
   $manage_service = $::hindsight::manage_service,
   $service_name   = $::hindsight::service_name,
   $run_dir        = $::hindsight::run_dir,
+  $content        = undef,
+  $source         = undef,
 ){
   $path = "${run_dir}/${target}.cfg"
 
@@ -28,10 +30,24 @@ define hindsight::plugin (
     }
 
     concat::fragment { $title:
-      target  => $path,
-      content =>  template('hindsight/plugin.cfg.erb'),
-      order   => $order,
+        target  => $path,
+        order   => $order,
     }
+
+    if $content {
+        Concat::Fragment<| title == $title |> {
+            content => $content,
+        }
+    } elsif $source {
+        Concat::Fragment<| title == $title |> {
+            source => $source,
+        }
+    } else {
+        Concat::Fragment<| title == $title |> {
+            content =>  template('hindsight/plugin.cfg.erb'),
+        }
+    }
+
     if $manage_service {
       Concat::Fragment<| title == $title |> {
         notify => Service[$service_name]
